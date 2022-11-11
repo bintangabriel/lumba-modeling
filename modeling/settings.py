a115f3f83068a608bdcde54228abc14e4b190225
Iqrar Agalosi Nureyza <misteriqrar@gmail.com>
2022-11-11 10:20:07 +0700

    Register apps

--- modeling/settings.py
+++ modeling/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v^q9aesoou-tz-fbr4%5a2yl+(*)&!&xz2s*h^th@-)ku8fla8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # project apps
+    'asynctrain',
+    'ml_model',
 ]
 
 MIDDLEWARE = [
